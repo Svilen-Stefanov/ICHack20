fd6b183a0810ebabcc1da848cf0b1b19feb0aee3
Svilen <svilen.ks@gmail.com>
2020-02-08 21:55:33 +0100

    add additional models, update APIs and restructure

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,4 @@
 import sqlalchemy
-import datetime
 from flask import Flask, request, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import DateTime
@@ -7,51 +6,7 @@ from sqlalchemy.orm import backref, relationship, session
 from flask_cors import CORS
 from flask import jsonify
 
-from dataclasses import dataclass
-from dataclasses_json import dataclass_json, LetterCase
-
-from typing import List
-
-
-########################################################################
-# Return dataclass Definitions
-@dataclass_json(letter_case=LetterCase.CAMEL)
-@dataclass
-class Skill:
-    skill_name: str
-    experience_level: int
-
-
-@dataclass_json(letter_case=LetterCase.CAMEL)
-@dataclass
-class Profile:
-    profile_id: int
-    name: str
-    image_url: str
-    institution: str
-    skills: List[Skill]
-    age: str
-    tokens: float
-
-
-@dataclass_json(letter_case=LetterCase.CAMEL)
-@dataclass
-class DashboardView:
-    profiles: List[Profile]
-
-
-@dataclass_json(letter_case=LetterCase.CAMEL)
-@dataclass
-class EnhancedProfile:
-    brief: Profile
-    description: str 
-
-
-########################################################################
-FAKE_PROFILES = {
-    "0": Profile(0, "Bobby Tables", "example.com", "Imperial College London", [Skill("Dancing", 3)], 27, 3.42),
-    "1": Profile(1, "Ms Bobby Tables", "exampl2e.com", "Imperial Collage London", [Skill("Maths", 1)], 28, 2.42)
-}
+from model_classes import Profile, Skill, User, Topic
 
 
 app = Flask(__name__)
@@ -60,13 +15,15 @@ CORS(app)
 
 # -------------------------------------------------------------
 # Database connection:
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ffgllqemmjnrnm:c07be32cb7851a450198e43a4009a092a7c43c3678e0dc8d3ad3e309ead09669@ec2-54-246-89-234.eu-west-1.compute.amazonaws.com:5432/daahtl1du1mh0'
+app.config[
+    'SQLALCHEMY_DATABASE_URI'] = 'postgres://ffgllqemmjnrnm:c07be32cb7851a450198e43a4009a092a7c43c3678e0dc8d3ad3e309ead09669@ec2-54-246-89-234.eu-west-1.compute.amazonaws.com:5432/daahtl1du1mh0'
+
 db = SQLAlchemy(app)
 
 
 # -------------------------------------------------------------
 # Database models:
-class User(db.Model):
+class DBUser(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True, server_default=sqlalchemy.text(
         'User_id_seq()'))
@@ -78,7 +35,7 @@ class User(db.Model):
     profile_pic = db.Column(db.String(500), nullable=True)
 
 
-class Subject(db.Model):
+class DBSubject(db.Model):
     __tablename__ = 'subjects'
     id = db.Column(db.Integer, primary_key=True, server_default=sqlalchemy.text(
         'subjects_id_seq()'))
@@ -86,15 +43,16 @@ class Subject(db.Model):
     description = db.Column(db.String(1000), nullable=True)
 
 
-class Topic(db.Model):
+class DBTopic(db.Model):
     __tablename__ = 'topics'
     id = db.Column(db.Integer, primary_key=True, server_default=sqlalchemy.text(
         'topics_id_seq()'))
+    name = db.Column(db.String(30), nullable=False)
     subject_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=False, unique=True)
-    subject = relationship('User', back_populates=__tablename__)
+    description = db.Column(db.String(1000), nullable=True)
 
 
-class Friend(db.Model):
+class DBFriend(db.Model):
     __tablename__ = 'friends'
     id = db.Column(db.Integer, primary_key=True, server_default=sqlalchemy.text(
         'friends_id_seq()'))
@@ -103,7 +61,7 @@ class Friend(db.Model):
     status = db.Column(db.Integer)
 
 
-class UserTopicMap(db.Model):
+class DBUserTopicMap(db.Model):
     __tablename__ = 'user_topic_map'
     id = db.Column(db.Integer, primary_key=True, server_default=sqlalchemy.text(
         'user_topic_map_id_seq()'))
@@ -112,21 +70,48 @@ class UserTopicMap(db.Model):
     expertise = db.Column(db.Integer, nullable=True)
 
 
+FAKE_PROFILES = {
+    "0": Profile(0, "Bobby Tables", "example.com", "Imperial College London", [Skill("Dancing", 3)], 27, 3.42),
+    "1": Profile(1, "Ms Bobby Tables", "exampl2e.com", "Imperial Collage London", [Skill("Maths", 1)], 28, 2.42)
+}
+
+
 # -------------------------------------------------------------
 # Main dashboard view.
 # The backend will aggregate what it thins is the best possible dashboard for the user.
-@app.route('/dashboard', methods=['GET'])
-def get_dashboard():
-    fake_return = DashboardView(FAKE_PROFILES.values())
-    return jsonify(fake_return)
+@app.route('/dashboard/<topic_id>', methods=['GET'])
+def get_dashboard(topic_id):
+    skilled_users = DBUserTopicMap.query.filter_by(topic_id=topic_id).all()
+    # do ML magic
+    best_mathes = skilled_users[:6]
+    # TODO: jsonify
+    return jsonify(best_mathes)
 
 
 # Get a specific profile with a give ID
 @app.route('/profile/<profile_id>', methods=['GET'])
 def get_profile(profile_id):
-    "TODO: Currently returns an error stacktrace if the profile_id could not be found"
-    fake_return = EnhancedProfile(FAKE_PROFILES[profile_id], "A very long description I cannot be bothered to type")
-    return jsonify(fake_return)
+    user = DBUser.query.filter_by(id=profile_id).first()
+    output_user = User(user.user_id, user.first_name, user.last_name, user.date_of_birth, user.institution, user.profile_pic)
+    return jsonify(output_user)
+
+
+# Get a list of all topics
+@app.route('/all_topics', methods=['GET'])
+def get_all_topics():
+    topics = DBTopic.query.all()
+    all_topics = []
+    for t in topics:
+        all_topics.append(Topic(t.id, t.name, t.subject_id, t.description))
+    return jsonify(all_topics)
+
+
+# Get a specific topic with a give topic ID
+@app.route('/topic/<topic_id>', methods=['GET'])
+def get_topic(topic_id):
+    t = DBTopic.query.filter_by(id=topic_id).first()
+    topic = Topic(t.id, t.name, t.subject_id, t.description)
+    return jsonify(topic)
 
 
 # Runs the app:
---
+++ backend/model_classes.py
@@ -0,0 +1,83 @@
+from dataclasses import dataclass
+from dataclasses_json import dataclass_json, LetterCase
+from typing import List
+
+
+# Return dataclass Definitions
+@dataclass_json(letter_case=LetterCase.CAMEL)
+@dataclass
+class Skill:
+    skill_name: str
+    experience_level: int
+
+
+@dataclass_json(letter_case=LetterCase.CAMEL)
+@dataclass
+class Profile:
+    profile_id: int
+    name: str
+    image_url: str
+    institution: str
+    skills: List[Skill]
+    age: str
+    tokens: float
+
+
+@dataclass_json(letter_case=LetterCase.CAMEL)
+@dataclass
+class EnhancedProfile:
+    brief: Profile
+    description: str
+
+
+@dataclass_json(letter_case=LetterCase.CAMEL)
+@dataclass
+class DashboardView:
+    profiles: List[Profile]
+
+
+@dataclass_json(letter_case=LetterCase.CAMEL)
+@dataclass
+class User:
+    user_id: int
+    first_name: str
+    last_name: str
+    date_of_birth: str
+    institution: str
+    description: str
+    profile_pic: str
+
+
+@dataclass_json(letter_case=LetterCase.CAMEL)
+@dataclass
+class Subject:
+    subject_id: int
+    name: str
+    description: str
+
+
+@dataclass_json(letter_case=LetterCase.CAMEL)
+@dataclass
+class Topic:
+    topic_id: int
+    name: str
+    subject_id: int
+    description: str
+
+
+@dataclass_json(letter_case=LetterCase.CAMEL)
+@dataclass
+class Friend:
+    friendship_id: int
+    user_id1: int
+    user_id2: int
+    status: str     # MAP FROM INT TO STRING
+
+
+@dataclass_json(letter_case=LetterCase.CAMEL)
+@dataclass
+class UserTopicMap:
+    skill_id: int
+    user_id: int
+    topic_id: int
+    expertise: str  # MAP FROM INT TO STRING
