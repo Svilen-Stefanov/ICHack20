f4dd8ed111667f0fb84e9db10423fcdafbfeee67
Alberto Spina <alberto.spina15@imperial.ac.uk>
2020-02-08 15:47:02 +0000

    package.json: Add minimal routing to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Switch, Route } from 'react-router-dom'
 
 import Navbar from './Navbar/Navbar';
 import Profiles from './Profiles/Profiles';
@@ -12,7 +13,12 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <Profiles />
+      <Switch>
+        <Route exact path='/' component={Profiles} />
+        <Route path='/test1/:number' component={Profiles} />
+        <Route path='/test2/:number' component={Profiles} />
+      </Switch>
+
     </div>
   );
 }
