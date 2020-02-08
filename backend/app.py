2601da7adc3b9448932457e04ee9269068799f6f
df4517 <df4517@ic.ac.uk>
2020-02-08 14:35:51 +0000

    Added db connection to app.py.

--- backend/app.py
+++ backend/app.py
@@ -4,13 +4,16 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 app = Flask(__name__)
+# Allow Cross-origin policy on all endpoints
 CORS(app)
+
+# Database connection:
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ffgllqemmjnrnm:c07be32cb7851a450198e43a4009a092a7c43c3678e0dc8d3ad3e309ead09669@ec2-54-246-89-234.eu-west-1.compute.amazonaws.com:5432/daahtl1du1mh0'
+db = SQLAlchemy(app)
 
 @app.route('/test', methods=['GET'])
 def test():
     return "API works! :D"
-
-
 
 # Runs the app:
 if __name__ == '__main__':
