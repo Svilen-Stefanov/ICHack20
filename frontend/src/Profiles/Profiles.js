7b32be9d4c9eb91ad2906238f9b9a4a3e1265160
Ed <ed@Eds-MacBook-Air.local>
2020-02-08 17:43:03 +0000

    frontend/: Add minimal header impl

--- frontend/src/Profiles/Profiles.js
+++ frontend/src/Profiles/Profiles.js
@@ -8,11 +8,17 @@ import "./Profiles.css";
 
 class Profiles extends Component {
     componentDidMount() {
-        axios.get('http://127.0.0.1:5000/dashboard')
-        .then(res => {
-            const profiles = res.data;
-            console.log(profiles);
-        });
+        axios.get('http://127.0.0.1:5000/dashboard',
+            {
+                headers: {
+                    'Account-Id': 0
+                }
+            })
+            .then(res => {
+
+                const profiles = res.data;
+                console.log(profiles);
+            });
     }
     static defaultProps = {
         profiles: [
@@ -21,14 +27,14 @@ class Profiles extends Component {
                 age: "20",
                 imgUrl: "https://images.unsplash.com/photo-1517070208541-6ddc4d3efbcb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
                 institution: "Imperial College London",
-                skills: [ 
+                skills: [
                     {
-                        skill_name: "Maths", 
-                        experience_level: "Intermediate" 
-                    }, 
+                        skill_name: "Maths",
+                        experience_level: "Intermediate"
+                    },
                     {
-                        skill_name: "Physics", 
-                        experience_level: "Beginner" 
+                        skill_name: "Physics",
+                        experience_level: "Beginner"
                     }
                 ]
             },
@@ -37,24 +43,24 @@ class Profiles extends Component {
                 age: "20",
                 imgUrl: "https://images.unsplash.com/photo-1517070208541-6ddc4d3efbcb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
                 institution: "Imperial College London",
-                skills: [ 
+                skills: [
                     {
-                        skill_name: "Maths", 
-                        experience_level: "Intermediate" 
-                    }, 
+                        skill_name: "Maths",
+                        experience_level: "Intermediate"
+                    },
                     {
-                        skill_name: "Physics", 
-                        experience_level: "Beginner" 
+                        skill_name: "Physics",
+                        experience_level: "Beginner"
                     }
                 ]
             }
-            
+
         ]
     }
-    
+
     render() {
         const profiles = this.props.profiles.map(profile => (
-            <ProfileCard key={uuid()} data = {profile}/>
+            <ProfileCard key={uuid()} data={profile} />
         ));
         return (
             <main className="Profiles-container">
